6e6c93fd7686717cc8e73018d34d5ce0247fef84
Abhishek S Negi <negibankai@gmail.com>
2022-08-04 23:05:53 +0530

    added multiply code

---
+++ sumjava/Multiply.java
@@ -0,0 +1,12 @@
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 5;
+int b= 5;
+  
+System.out.println("the sum is " + (a*b));
+	}
+
+}
